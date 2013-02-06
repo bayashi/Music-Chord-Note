b49f8cc06f68bd40339dd7722970f03224c68e33
bayashi <bayashi@cpan.org>
2013-02-06 23:30:52 +0900

    bumped version

--- lib/Music/Chord/Note.pm
+++ lib/Music/Chord/Note.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp qw( croak );
 
-our $VERSION = '0.061';
+our $VERSION = '0.07';
 
 my @tone_list = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B',
                  'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B');
